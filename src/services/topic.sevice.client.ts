f7f1c681ac9f09531f1b71884ba74c7d8a2b6cda
amanrayat <amanrayat32@gmail.com>
2018-11-09 17:29:12 -0500

    adding dump services

---
+++ src/services/topic.sevice.client.ts
@@ -0,0 +1,8 @@
+import {Injectable} from '@angular/core';
+
+@Injectable
+export class TopicServiceClient {
+  findAllTopic() {}
+  findTopicById (topicId) {}
+  findTopicForLesson (lessonId) {}
+}
---
+++ src/services/widget.service.client.ts
@@ -0,0 +1,9 @@
+import {Injectable} from '@angular/core';
+
+@Injectable
+export class WidgetServiceClient {
+  findAllWidgets() {}
+  findWidgetsForTopic(topicId) {}
+  findWidgetById(widgetId) {}
+
+}
