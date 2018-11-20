cb370b4af14f4b53f82170ebc14ae0158c21e54c
amanrayat <amanrayat32@gmail.com>
2018-11-20 17:26:53 -0500

    updating server.js again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/simple-blog-app-angular'));
+app.use(express.static('./dist/CS5610-angular'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join('./dist/CS5610-angular/index.html'));
