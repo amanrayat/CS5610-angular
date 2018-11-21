547c26369d227d60d40fd4d8c7221cd420d71042
amanrayat <amanrayat32@gmail.com>
2018-11-20 20:01:45 -0500

    updating the server.js 3

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/CS5610-angular'));
+app.use(express.static(__dirname + '/dist/whiteboard-fall2018-client-angular'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/CS5610-angular/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/whiteboard-fall2018-client-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
